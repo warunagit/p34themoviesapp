dac4126125b81f9595baf8fc1e85b422c6d77a2c
warunagit <waruna.git@gmail.com>
2025-03-20 16:41:01 +0200

    dependancies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,6 +35,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +50,44 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //lifecycle
+    val lifecycle_version = "2.8.7"
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    // Saved state module for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
+    // Annotation processor
+    ksp("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+
+    //room
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    //Kotlin Symbol Processing
+    ksp("androidx.room:room-compiler:$room_version")
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    //coroutines
+    val coroutine_version = "2.6.1"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutine_version")
+
+    //Dagger
+    val dagger_version = "2.56"
+    implementation ("com.google.dagger:dagger:$dagger_version")
+    annotationProcessor ("com.google.dagger:dagger-compiler:$dagger_version")
+
+    //Retrofit
+    val retrofit_version = "2.12.1"
+    implementation ("com.squareup.retrofit2:retrofit:$retrofit_version")
+    //GSON
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    //OKHTTP3
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    //GLIDE: Image loading library for smooth scrolling
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    ksp ("com.github.bumptech.glide:compiler:4.16.0")
 }
--- app/src/main/java/com/cycolabs/p34themoviesapp/MainActivity.kt
+++ app/src/main/java/com/cycolabs/p34themoviesapp/MainActivity.kt
@@ -7,14 +7,34 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    //app uder layers
+    //
+    //|-Data-|-Repository [Local data source Entity, SQLite<->DAO]
+    //|      |-Remote Data Source (Web Service <-> API)
+    //|-UseCase (Domain)
+    //|-[View-Model[Live-Data[Model]]]
+    //|-View (Activity/Fragment)
+    //When getting app big, there will be more ViewModels
+    //Somethings will located in different VM classes
+    //To avoid this complexity,
+    // can create various UseCase classes to reduce code duplication
+    // and complication.
+    //Presentation - Interact with user - Activity, Fragment, ViewModel
+    //Use Case - layer where define user's works
+    //Domain - layer where logic operations performed.
+    //Eg: entities, value objects, exceptions, logic operations
+    //Data - Source of all abstract data and models.
+    //Framework - Layer that interacts with SDK and provides custom implementations for data layer
+
+    //Top 5 movies are dynamics.
+    //should allow user to refresh and receive updated data from API
+    //Contains 3 categories, Movies, TV Shows, Artists
+    //For 3 categories, need 6 use cases. 3 for receiving and 3 for update data
+
+    //1-Add dependencies
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
